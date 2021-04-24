dea14b98403c1449677068a9524f1619445073aa
Raza <raza@thedatech.com>
2021-04-24 17:32:57 +0500

    all media url show

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -20,11 +20,14 @@ class ProductController extends Controller
             "brand"
             ])->paginate(12);
 
-        $response["error"]      =   "false";
-        $response["code"]       =   "200";
-        $response["message"]    =   "Operation Successfully.";
-        $response["url"]        =   url('api/products');
-        $response["products"]   =   $products;
+        $response["error"]              =   "false";
+        $response["code"]               =   "200";
+        $response["message"]            =   "Operation Successfully.";
+        $response["url"]                =   url('api/products');
+        $response["image-url"]          =   url("backend/images/products");
+        $response["gallery-image-url"]  =   url("backend/images/products");
+        $response["document-url"]       =   url("backend/product_document");
+        $response["products"]           =   $products;
 
         if(is_null($products)){
             return response()->json("Products not Found!",404);
@@ -39,10 +42,13 @@ class ProductController extends Controller
         # code...
             $product     =   Product::with(["product_grallery","document_product","category","brand"])->find($id);
 
-            $response["error"]      =   "false";
-            $response["code"]       =   "200";
-            $response["message"]    =   "Operation Successfully.";
-            $response["url"]        =   url('api/product/'.$id);
+            $response["error"]              =   "false";
+            $response["code"]               =   "200";
+            $response["message"]            =   "Operation Successfully.";
+            $response["url"]                =   url('api/product/'.$id);
+            $response["image-url"]          =   url("backend/images/products");
+            $response["gallery-image-url"]  =   url("backend/images/product_gallery");
+            $response["document-url"]       =   url("backend/product_document");
             $response["product"]    =   $product;
 
             if(is_null($product)){
