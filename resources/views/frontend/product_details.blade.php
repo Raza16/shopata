b9a68438bae352b9d83b110c90b2719e50e64e39
Raza <raza@thedatech.com>
2021-04-27 12:07:13 +0500

    add empty line

--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -23,18 +23,22 @@
                 <div class="all">
                     <div class="slider">
                         <div class="owl-carousel owl-theme main">
-                          {{-- @foreach($product as $image) --}}
+
                             <div style="background-image: url('{{$product->product_image ? asset('backend/images/products/'.$product->product_image) : asset('frontend/img/product_placeholder.jpg')}}'); background-size: contain;
-                                margin: auto; background-repeat: no-repeat;" class="item-box"></div>
+                                margin: auto; background-repeat: no-repeat;" class="item-box">
+                            </div>
 
                           @foreach($product_grallery as $image)
                             <div style="background-image: url('{{asset('backend/images/product_gallery/'.$image->image)}}');background-size: contain;margin: auto;background-repeat: no-repeat;" class="item-box"></div>
                             @endforeach
 
                         </div>
+
                         <div class="left nonl"><i class="ti-angle-left"></i></div>
                         <div class="right"><i class="ti-angle-right"></i></div>
+
                     </div>
+
                     <div class="slider-two">
                         <div class="owl-carousel owl-theme thumbs">
                             <div style="background-image: url('{{asset('backend/images/products/'.$product->product_image)}}');" class="item active"></div>
