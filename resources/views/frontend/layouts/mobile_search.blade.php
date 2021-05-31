40f03f53a597f0e810a301ea2baa105294b8a231
Raza <raza@thedatech.com>
2021-05-31 18:33:40 +0500

    ajax base search on mobile view

--- resources/views/frontend/layouts/mobile_search.blade.php
+++ resources/views/frontend/layouts/mobile_search.blade.php
@@ -1,8 +1,42 @@
 <div class="search_mob_wp">
-    <input type="text" class="form-control" placeholder="Search over {{count($product)}} products">
+    <input type="text" class="form-control" placeholder="Search over {{count($product)}} products" id="product_mob">
     <input type="submit" class="btn_1 full-width" value="Search">
 </div>
-<div class="custom-search-li-mob" hidden>
-    <li>Hello</li>
-    <li>Hi</li>
+
+<div id="product_list" style="position: absolute">
+
 </div>
+
+@push('script')
+<script type="text/javascript">
+    $(document).ready(function () {
+
+        $('#product_mob').on('keyup',function() {
+            var query = $(this).val();
+            var url = '{{ url('search') }}';
+            $.ajax({
+
+                url:url,
+
+                type:"GET",
+
+                data:{'product':query},
+
+                success:function (data) {
+
+                    $('#product_list').html(data);
+                }
+            });
+            // end of ajax call
+        });
+
+        $(document).on('click', 'li', function(){
+
+            var value = $(this).text();
+            $('#product').val(value);
+            $('#product_list').html("");
+        });
+    });
+</script>
+
+@endpush
